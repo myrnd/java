efff3ff5d28eef4467a54a8e79c449665fe95248
Vivek Kumar <myrnd.vivek@gmail.com>
2022-09-16 22:30:47 +0530

    Added polymorphism examples

---
+++ oops/polymorphism/MarioGame.java
@@ -0,0 +1,14 @@
+public class MarioGame implements GamingConsole {
+  public void up() {
+    System.out.println("Jump");
+  }
+  public void down() {
+    System.out.println("Goes into a hole");
+  }
+  public void left() {
+
+  }
+  public void right() {
+    System.out.println("Go Forwards");
+  }
+}
